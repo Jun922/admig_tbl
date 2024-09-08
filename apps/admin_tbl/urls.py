b7b9e163a9ece9b73c131414defeabc4d3401881
Jun922 <junjunchi620@gmail.com>
2024-09-08 23:57:40 +0900

    lesson list

--- apps/admin_tbl/urls.py
+++ apps/admin_tbl/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 app_name= "admin_tbl"
 urlpatterns = [
+    # student part
     path("", views.StudentListView.as_view(), name="student_list"),
     path("detail/<int:pk>", views.StudentDetail.as_view(), name="student_detail"),
     path("create", views.StudentCreate.as_view(), name="student_create"),
     path("update/<int:pk>", views.StudentUpdate.as_view(), name="student_update"),
     path("delete/<int:pk>", views.StudentDelete.as_view(), name="student_delete"),
+
+    # lesson part
+    path("list/<int:pk>", views.LessonListView.as_view(), name="lesson_list"),
 ]
--- apps/admin_tbl/views.py
+++ apps/admin_tbl/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import resolve_url
 from django.urls import reverse_lazy
 from django.contrib import messages
 
-from .models import Student
+from .models import Student, Lesson
 from .forms import StudentForm
 
 
+# ------------------------------
+#         student part
+# ------------------------------
 class StudentListView(ListView):
     template_name = "admin_tbl/student_list.html"
     model = Student
@@ -51,3 +54,58 @@ class StudentDelete(DeleteView):
 
     def get_success_url(self):
         return reverse_lazy("admin_tbl:student_list")
+
+
+
+# ------------------------------
+#          lesson part
+# ------------------------------
+class LessonListView(ListView):
+    template_name = "admin_tbl/lesson_list.html"
+    model = Student
+    context_object_name = "lessons"
+    
+    def get_queryset(self):
+        student_id = self.kwargs['pk']
+        queryset = Lesson.objects.filter(student_id=student_id).all()
+        return queryset
+    
+    # def get_queryset(self):
+    #     queryset = Student.objects.prefetch_related("lesson")
+    #     return queryset
+
+
+# class StudentDetail(DetailView):
+#     template_name = "admin_tbl/student_detail.html"
+#     model = Student
+    
+#     def get_queryset(self):
+#         queryset = Student.objects.prefetch_related("lesson")
+#         return queryset
+
+
+# class StudentCreate(CreateView):
+#     template_name = "admin_tbl/student_form.html"
+#     model = Student
+#     form_class = StudentForm
+#     success_url = reverse_lazy("admin_tbl:student_list")
+
+#     def get_success_url(self):
+#         messages.success(self.request, "記事を投稿しました。")
+#         return resolve_url("admin_tbl:student_list")
+
+
+# class StudentUpdate(UpdateView):
+#     template_name = "admin_tbl/student_form.html"
+#     model = Student
+#     form_class = StudentForm
+
+#     def get_success_url(self):
+#         return reverse_lazy("admin_tbl:student_list")
+
+
+# class StudentDelete(DeleteView):
+#     model = Student
+
+#     def get_success_url(self):
+#         return reverse_lazy("admin_tbl:student_list")
