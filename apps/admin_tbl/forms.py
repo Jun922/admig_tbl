06b37e13b22b1c963c84915b2b85c79acbca0aad
Jun922 <junjunchi620@gmail.com>
2024-09-11 00:28:54 +0900

    create/update html

--- apps/admin_tbl/forms.py
+++ apps/admin_tbl/forms.py
@@ -7,9 +7,33 @@ class StudentForm(forms.ModelForm):
         model = Student
         fields = ("name", "age", "sex", "job", "memo")
         widgets = {
-                    "name": forms.TextInput(attrs={"placeholder":"記入例：山田　太郎"}),
-                    "age": forms.NumberInput(attrs={"min":1}),
-                    "sex": forms.RadioSelect(),
-                    "job": forms.TextInput(attrs={"placeholder":"記入例：プログラマー"}),
-                    "memo": forms.Textarea(attrs={"rows":4}),
+                    "name": forms.TextInput(
+                        attrs={
+                            "class": "student-form student-name",
+                            "placeholder": "記入例：山田　太郎",
+                        }
+                    ),
+                    "age": forms.NumberInput(
+                        attrs={
+                            "class": "student-form student-age",
+                            "min": 1,
+                        }
+                    ),
+                    "sex": forms.RadioSelect(
+                        attrs={
+                            "class": "student-form student-sex",
+                        }
+                    ),
+                    "job": forms.TextInput(
+                        attrs={
+                            "class": "student-form student-job",
+                            "placeholder": "記入例：プログラマー",
+                        }
+                    ),
+                    "memo": forms.Textarea(
+                        attrs={
+                            "class": "student-form student-memo",
+                            "rows": 4,
+                        }
+                    ),
         }
